80c33ca26310fddeb18fe98ac5de73bbdb823671
Lincoln Swaine-Moore <lswainemoore@gmail.com>
2017-04-21 05:34:47 -0400

    having trouble with some of the serialization for session

---
+++ interface/genvote_copy.py
@@ -0,0 +1,254 @@
+from petlib.ec import EcGroup, EcPt
+from petlib.bn import Bn
+from petlib.ecdsa import do_ecdsa_sign, do_ecdsa_setup
+from hashlib import sha512
+from genzkp import ZKProof, ZKEnv, ConstGen, Sec
+from itertools import chain
+from collections import defaultdict, Counter
+import binascii
+import json
+import uuid
+import random
+import qrcode
+
+G = EcGroup(934)
+order = G.order()
+h = G.generator()
+sleeve = "nothing_up_my_sleeve"
+g = G.hash_to_point(sleeve.encode('utf-8'))
+
+#secret!!!
+sig_key = order.random()
+kinv_rp = do_ecdsa_setup(G, sig_key)
+#not secret
+ver_key = sig_key * h
+
+def commit(a, r):
+	return a * h + r * g
+
+
+def genRealCommitments(vote, k, R):
+	real_commitment = commit(vote, R)
+	rb = [order.random() for i in range(k)]
+	masks = [commit(vote, R + rb[i]) for i in range(k)]
+	return real_commitment, masks, rb
+
+def genFakeCommitments(dummy_challenges, k, real_vote, R):
+	randoms = {}
+	commitments = defaultdict(list)
+	for vote, challenge_str in dummy_challenges.items():
+		challenge = challengeHash(challenge_str, k)
+		randoms[vote] = [order.random() for i in range(k)]
+		challenge_num = int(challenge, 16)
+		for i in range(k):
+			if (challenge_num & 1) == 1:
+				commitments[vote].append(commit(real_vote, R + randoms[vote][i]))
+			else:
+				commitments[vote].append(commit(vote, R + randoms[vote][i]))
+			challenge_num >>= 1
+	return commitments, randoms
+
+def challengeHash(challenge, k):
+	assert(k <= 512)
+	m = sha512(challenge.encode('utf-8')).hexdigest()
+	nbytes = k // 8
+	nbits = k % 8
+	byte_trunc_m = m[:2*nbytes]
+	if nbits > 0:
+		bit_trunc_m = m[2*nbytes: 2*nbytes + 2]
+		bit_num = (int(bit_trunc_m, 16) >> (8 - nbits)) << (8 - nbits)
+		byte_trunc_m += format(bit_num, 'x')
+	return byte_trunc_m
+
+def answerChallenges(challenges, randoms, k, R):
+	answers = defaultdict(list)
+	for vote, challenge_str in challenges.items():
+		challenge = challengeHash(challenge_str, k)
+		challenge_num = int(challenge, 16)
+		for i in range(k):
+			if (challenge_num & 1) == 1:
+				answers[vote].append(randoms[vote][i])
+			else:
+				answers[vote].append(R + randoms[vote][i])
+			challenge_num >>= 1
+	return answers
+
+
+K = 128
+
+d = {'ALICE': 1, 'BETTY': 2, 'CHARLIE': 3}
+rev_d = {v:k for k,v in d.items()}
+
+candidates = [d['ALICE'], d['BETTY'], d['CHARLIE']]
+
+dictionary_words = None
+with open('/usr/share/dict/words') as f:
+	dictionary_words = list(map(str.strip,f.readlines()))
+
+def getRandomWord():
+	return random.choice(dictionary_words)
+
+def serializeEcPts(d):
+	iteror = None
+	new_d = None
+	if isinstance(d, dict):
+		new_d = dict(d)
+		iteror = new_d.items()
+	elif isinstance(d, list):
+		new_d = list(d)
+		iteror = enumerate(new_d)
+	else:
+		return d
+	for key, value in iteror:
+		if isinstance(value, EcPt):
+			new_d[key] = EcPtToStr(value)
+		elif isinstance(value, dict):
+			new_d[key] = serializeEcPts(value)
+		elif isinstance(value, list):
+			new_d[key] = list(map(EcPtToStr, value))
+		elif isinstance(value, tuple):
+			new_d[key] = tuple(map(EcPtToStr, value))
+	return new_d
+
+def castVote(voter_id, candidate):
+	DC = {}
+	R = order.random()
+	for non_vote in filter(lambda l: l != candidate, candidates):
+		DC[non_vote] = ' '.join([getRandomWord() for i in range(4)])
+	rc, masks, rb = genRealCommitments(candidate, K, R)
+	commitments, randoms = genFakeCommitments(DC, K, candidate, R)
+	randoms[candidate] = rb
+	commitments[candidate] = masks
+	cmt_list = []
+	for sk in sorted(commitments):
+		cmt_list.append(commitments[sk])
+	everything = challengeHash(''.join(map(str,[rc] + list(chain(cmt_list)))), K) #alphabetize this
+	rx = order.random()
+	x = commit(Bn.from_hex(everything), rx)
+	DC[candidate] = ' '.join([getRandomWord() for i in range(4)])  #random challenge real vote
+	answers = answerChallenges(DC, randoms, K, R)
+	verifyCommitment(x, rc, cmt_list, rx)
+	challenge_dict = {candidate: {'challenge': DC[candidate], 'answer': list(map(str,answers[candidate])), 'proof': commitments[candidate]} for candidate in DC}
+	receipt = serializeEcPts({'voter_id': voter_id, 'challenges': challenge_dict, 'vote_commitment': rc, 'rx': str(rx), 'commitment_to_everything': x})
+	sig = do_ecdsa_sign(G, sig_key, EcPtToStr(x).encode('utf-8'), kinv_rp)
+	signed_cmt = '_'.join((EcPtToStr(x), hex(sig[0])[2:], hex(sig[1])[2:]))
+	qr = qrcode.QRCode(
+			version = 1,
+			error_correction = qrcode.constants.ERROR_CORRECT_L,
+			box_size = 4,
+			border = 4,
+	)
+	qr.add_data(signed_cmt)
+	qr.make()
+	img = qr.make_image()
+	img.save('qrcodes/to_print.png')
+	return (candidate, rc, R, everything, str(x), answers, receipt)
+
+def verifyChallenge(cd, vote_commitment):
+	vc = strToEcPt(vote_commitment, G)
+	for candidate in cd:
+		answers = list(map(Bn.from_decimal,cd[candidate]['answer']))
+		proofs = list(map(lambda l: strToEcPt(l, G), cd[candidate]['proof']))
+		challenge_bits = int(challengeHash(cd[candidate]['challenge'], K), 16)
+		for i in range(K):
+			if (challenge_bits & 1) == 0:
+				assert(proofs[i] == commit(candidate, answers[i]))
+			else:
+				assert(proofs[i] == vc + answers[i] * g)
+			challenge_bits >>= 1
+
+
+
+
+def verifyCommitment(x, vote, commitments, rx):
+	everything = challengeHash(''.join(map(str,[vote] + list(chain(commitments)))), K) #alphabetize this
+	result = commit(Bn.from_hex(everything), rx)
+	assert(result == x)
+
+def permuteAndMask(votes, vote_commitments):
+	pi = list(range(len(votes)))
+	random.shuffle(pi)
+	maskers = [order.random() for i in range(len(votes))]
+	pm_vote_commitments = [vote_commitments[pi[i]] +  maskers[pi[i]] * g for i in range(len(votes))]
+	return pm_vote_commitments, maskers, pi
+
+def openMaskedCommitments(votes, maskers, r, pi):
+	return [(votes[pi[i]], str(r[pi[i]] + maskers[pi[i]])) for i in range(len(votes))]
+
+def verifyMaskedCommitments(pm_vote_commitments, comm_pairs, tally):
+	permuted_votes = []
+	for comm, unmask in zip(pm_vote_commitments, comm_pairs):
+		permuted_votes.append(unmask[0])
+		assert(comm == commit(unmask[0], Bn.from_decimal(unmask[1])))
+	assert(tally == Counter(permuted_votes))
+
+def verifyPermutation(pm_vote_commitments, vote_commits, maskers, pi):
+	assert(pm_vote_commitments == [vote_commits[pi[i]] + maskers[pi[i]] * g for i in range(len(votes))])
+
+
+def EcPtToStr(pt):
+	if not isinstance(pt, EcPt):
+		return pt
+	return binascii.hexlify(pt.export()).decode('utf8')
+
+def strToEcPt(s, group):
+	return EcPt.from_binary(binascii.unhexlify(s), group)
+
+
+def doFiatShamir(votes, vote_commits, randoms, tally):
+	pmv_masks_pi = [permuteAndMask(votes, vote_commits) for i in range(K)]
+	proofs = [' '.join(map(EcPtToStr, p[0])) for p in pmv_masks_pi]
+	masks = [' '.join(map(lambda l: l.hex(), p[1])) for p in pmv_masks_pi]
+	pis = [' '.join(map(str, p[2])) for p in pmv_masks_pi]
+	proof_str = ''.join(proofs)
+	beacon = int(challengeHash(proof_str, K), 16)
+	proof_l = []
+	for i in range(K):
+		p_dict = {}
+		if (beacon & 1) == 0:
+			p_dict['proof_type'] = 'unmask'
+			p_dict['maskers'] = masks[i]
+			p_dict['pi'] = pis[i]
+			p_dict['pm_vote_commitments'] = proofs[i]
+			verifyPermutation(list(map(lambda s: strToEcPt(s,G) , proofs[i].split(' '))), vote_commits, list(map(Bn.from_hex,masks[i].split(' '))), list(map(int, pis[i].split(' '))))
+		else:
+			opened = openMaskedCommitments(votes, list(map(Bn.from_hex,masks[i].split(' '))), randoms, list(map(int,pis[i].split(' '))))
+			p_dict['proof_type'] = 'open'
+			#print(opened)
+			p_dict['comm_pairs'] = serializeEcPts(opened)
+			p_dict['pm_vote_commitments'] = proofs[i]
+			verifyMaskedCommitments(list(map(lambda s: strToEcPt(s,G), proofs[i].split(' '))), opened, tally)
+		beacon >>= 1
+		proof_l.append(dict(p_dict))
+	return proof_l
+
+
+
+
+# vote_data = [castVote(str(uuid.uuid4()), random.randint(1,3)) for i in range(10)]
+# vote_data.sort(key = lambda x: x[6]['voter_id'])
+# votes = [v[0] for v in vote_data]
+# vote_commits = [v[1] for v in vote_data]
+# randoms = [v[2] for v in vote_data]
+
+# receipts = [v[6] for v in vote_data]
+# verifyChallenge(receipts[0]['challenges'], receipts[0]['vote_commitment'])
+# #print(receipts_json)
+
+# tally = Counter(votes)
+# print(tally)
+
+# proofs = doFiatShamir(votes, vote_commits, randoms, tally)
+# big_dict = {'G': '934', 'sleeve': sleeve, 'g': EcPtToStr(g), 'h': EcPtToStr(h), 'precinct-id': '0', 'receipts': receipts, 'tally': tally, 'proofs': proofs}
+# json_str = json.dumps(big_dict)
+# #print(json.dumps(big_dict))
+# for candidate in tally:
+# 	print("%s: %d" % (rev_d[candidate], tally[candidate]))
+# with open("./verification.json", 'w') as f:
+# 	f.write(json_str)
+
+
+
+
+
+
--- interface/interface.py
+++ interface/interface.py
@@ -4,39 +4,22 @@ from flask import request
 from flask import session
 import numpy
 
+import os
+from RedisSession import RedisSessionInterface
+
+# using this version which has code at bottom commented out to speed things up
+import genvote_copy as genvote
+
+import uuid
+from itertools import chain
+from petlib.bn import Bn
+from petlib.ec import EcGroup, EcPt
+
+
 app = Flask(__name__)
-app.secret_key = "should be more secret than this"
+app.secret_key = "should be more secret than this" # OBVIOUSLY NEEDS TO CHANGE
 
-# class Candidate:
-# 	def __init__(self, name, party, office):
-# 		self.name = name
-# 		self.party = party
-# 		self.office = office
-
-# 		# this should probably be a hash of name, party, office, or something
-# 		self.candID = 1
-
-# 	def print_name(self):
-# 		return self.name + " (" + self.party + ")"
-
-# class Race:
-# 	def __init__(self, office):
-# 		self.office = office
-
-# 		# this should probably be a hash of office, or something
-# 		self.raceID = 1
-# 		self.cands = []
-
-# 	def add_cand(self, name, party):
-# 		self.cands.append(Candidate(name,party, self.office))
- 
-# def initialize_race():
-	
-#     r = Race("pres")
-#     r.add_cand("Bertha", "Blue Party")
-#     r.add_cand("Gilbert", "Green")
-#     r.add_cand("Rover", "Red")
-#     return r	
+# app.session_interface = RedisSessionInterface()
 
 # written because of a weird bug where a dictionary persisted in session had its keys switch from ints to unicode
 def convert_keys_to_int(d):
@@ -51,68 +34,173 @@ def reset_dict_keys():
 	session["rev_d"] = convert_keys_to_int(session["rev_d"])
 	session["challenges"] = convert_keys_to_int(session["challenges"])
 
-def initialize():
+# based on serializeEcPts
+def serializeBns(d):
+	iteror = None
+	new_d = None
+	if isinstance(d, Bn):
+		return hex(d)
+	elif isinstance(d, dict):
+		new_d = dict(d)
+		iteror = new_d.items()
+	elif isinstance(d, list):
+		new_d = list(d)
+		iteror = enumerate(new_d)
+	else:
+		return d
+	for key, value in iteror:
+		if isinstance(value, Bn):
+			new_d[key] = serializeBns(value)
+		elif isinstance(value, dict):
+			new_d[key] = serializeBns(value)
+		elif isinstance(value, list):
+			new_d[key] = list(map(serializeBns, value))
+		elif isinstance(value, tuple):
+			new_d[key] = tuple(map(serializeBns, value))
+	return new_d
 
-	# cands = []
-	# cands.append("Bertha (Blue Party)")
-	# cands.append("Gilbert (Green Party)")
-	# cands.append("Rover (Red Party)")
 
-	# cands = {'ALICE': 1, 'BETTY': 2, 'CHARLIE': 3}
+# serialize these Bn and EctPt -based objects before putting them in session
+def persist_tricky_objects(R = None, rc = None, masks = None, rb = None, commitments = None,
+	randoms = None, cmt_list = None, everything = None, rx = None, x = None, answers = None, receipt = None):
 
+	session["R"] = serializeBns(R)
+	
+	session["rc"] = genvote.EcPtToStr(rc)
+
+	session["masks"] = genvote.serializeEcPts(masks)
+
+	session["rb"] = serializeBns(rb)
+
+	session["commitments"] = genvote.serializeEcPts(commitments)
+
+	session["randoms"] = serializeBns(randoms)
+
+	session["cmt_list"] = genvote.serializeEcPts(cmt_list)
+
+	session["everything"] = everything
+
+	session["rx"] = serializeBns(rx)
+
+	session["x"] = genvote.EcPtToStr(x)
+
+	# THIS ISN'T CORRECT YET!
+	print("answers: ", type(answers))
+	session["answers"] = answers
+	print("receipt: ", type(receipt))
+	session["receipt"] = receipt
+
+
+# convert from serialized form in session to original form
+# currently, just returns objects as they appear in session!
+def desist_tricky_objects():
+
+	# weirdly, R = Bn.from_hex(session["R"]) gives a BN Error exception
+	R = session["R"]
+	rc = session["rc"]
+	masks = session["masks"]
+	rb = session["rb"]
+	commitments = session["commitments"]
+	randoms = session["randoms"]
+	cmt_list = session["cmt_list"]
+	everything = session["everything"]
+	rx = session["rx"]
+	x = session["x"]
+	answers = session["answers"]
+	receipt = session["receipt"]
+
+	return R, rc, masks, rb, commitments, randoms, cmt_list, everything, rx, x, answers, receipt
+
+def setup_machine():
+
+	# election information
+	# (consider either reading this from config file, 
+	# allowing user to input at setup stage)
 	session["d"] = {'ALICE': 1, 'BETTY': 2, 'CHARLIE': 3}
 	session["rev_d"] = {v:k for k,v in session["d"].items()}
 	session["candidates"] = [session["d"]['ALICE'], session["d"]['BETTY'], session["d"]['CHARLIE']]
-
+	
+	# for autogenerating challenges
 	# with open('/usr/share/dict/words') as f:
 	# 	session["dictionary_words"] = list(map(str.strip,f.readlines()))
-
 	session["dictionary_words"] = ["hello", "these", "are", "challenge", "words", "which", "should", "later", "be", "replaced", "by", "a", "dictionary"]
-
 	session["lenChallenge"] = 3
+
+	# where votes are stored
+	# gradually filled as more users use machine
+	session["vote_data"] = []
+
+# values that should be refreshed every time there is a new voter
+def new_voter():
+
+	session["voter_id"] = str(uuid.uuid4())
 	session["chosen"] = None
 	session["challenges"] = {}
 	session["chosen_challenge"] = None
 
+	# new variables from castVote
+	# using session["chosen"] instead of candidate, and session["challenges"] instead of DC
+
+	# calling this with no arguments should set all the rest of the variables we need to persist to None
+	persist_tricky_objects()
+
+	# NOW TAKEN CARE OF IN PERSIST_TRICKY_OBJECTS
+	# session["R"] = None
+	# session["rc"] = None
+	# session["masks"] = None
+	# session["rb"] = None
+	# session["commitments"] = None
+	# session["randoms"] = None
+	# session["cmt_list"] = None
+	# session["everything"] = None
+	# session["rx"] = None
+	# session["x"] = None
+	# session["answers"] = None
+	# session["receipt"] = None
+
+	# DO NOT NEED TO BE PERSISTED ACROSS VIEWS
+	# session["challenge_dict"] = None
+	# session["sig"] = None
+	# session["signed_cmt"] = None
+	# session["qr"] = None
+	# session["img"] = None
+
 @app.route("/")
+def start_machine():
+
+	setup_machine()
+
+	return render_template("setup.html")
+
+@app.route("/stage0")
 def stage0():
-	return render_template("stage0.html")
+
+	new_voter()
+
+	return render_template("stage0.html", voter_id = session["voter_id"])
 
 @app.route("/stage1")
 def stage1():
 
-	# initialize session
-    
-    # session["words"] = ["hello", "these", "are", "challenge", "words", "which", "should", "later", "be", "replaced", "by", "a", "dictionary"]
-    # session["lenChallenge"] = 3
-    # session["chosen"] = None
-    # session["challenges"] = None
-    # session["chosen_challenge"] = None
+	reset_dict_keys()
 
-
-    initialize()
-
-    # i = Interaction(r)
-    # i.start()
-    return render_template("stage1.html", candidates = session["candidates"], cand_dict = session["rev_d"])
+	print ("candidates: ", session["candidates"])
+	print ("rev_d: ", session["rev_d"])
+	return render_template("stage1.html", candidates = session["candidates"], cand_dict = session["rev_d"])
 
 @app.route("/stage2", methods = ["POST"])
-# @app.route("/")
 def stage2():
-	
 
-	# super annoying that I have to do this for some reason
+	# super annoying that I have to do this
 	reset_dict_keys()
 
 	session["chosen"] = int(request.form["chosen"])
 
 	# should never be triggered because we check in stage2.html
-	assert(session["chosen"] > 0 and session["chosen"] <= len(session["candidates"]))
+	# assert(session["chosen"] > 0 and session["chosen"] <= len(session["candidates"]))
 	
 	challenges = {i:" ".join(numpy.random.choice(session["dictionary_words"], session["lenChallenge"], replace = False)) for i in session["candidates"]}
 	challenges[session["chosen"]] = None
-	# print("I'm in stage 2")
-	# print(challenges)
 
 
 	return render_template("stage2.html", candidates = session["candidates"], cand_dict = session["rev_d"], chosen = session["chosen"], challenges = challenges)
@@ -121,20 +209,66 @@ def stage2():
 # @app.route("/")
 def stage3():
 
-	# super annoying that I have to do this for some reason
+	# super annoying that I have to do this
 	reset_dict_keys()
 	
 	session["challenges"] = {}
 	for i in session["candidates"]:
 		if i != session["chosen"]:
 			session["challenges"][i] = request.form["challenge" + str(i)]
-		else:
-			session["challenges"][i] = None
-	# print("I'm in stage 3")
+		# else:
+		# 	session["challenges"][i] = None
 	# print(session["challenges"])
 
+	# NEW
+	R = genvote.order.random()
+	rc, masks, rb = genvote.genRealCommitments(session["chosen"], genvote.K, R)
+	commitments, randoms = genvote.genFakeCommitments(session["challenges"], genvote.K, session["chosen"], R)
+	randoms[session["chosen"]] = rb
+	commitments[session["chosen"]] = masks
+	cmt_list = []
+	for sk in sorted(commitments):
+		cmt_list.append(commitments[sk])
+	everything = genvote.challengeHash(''.join(map(str,[rc] + list(chain(cmt_list)))), genvote.K)
+	rx = genvote.order.random()
+	x = genvote.commit(Bn.from_hex(everything), rx)
 
-	
+
+	persist_tricky_objects(R, rc, masks, rb, commitments, randoms, cmt_list, everything, rx, x, answers = None, receipt = None)
+
+	# print(session["challenges"])
+	# session["R"] = genvote.order.random()
+	# session["rc"], masks, rb = genvote.genRealCommitments(session["chosen"], genvote.K, session["R"])
+	# session["commitments"], session["randoms"] = genvote.genFakeCommitments(session["challenges"], genvote.K, session["chosen"], session["R"])
+	# session["randoms"][session["chosen"]] = rb
+	# session["commitments"][session["chosen"]] = masks
+	# session["cmt_list"] = []
+	# for sk in sorted(session["commitments"]):
+	# 	session["cmt_list"].append(session["commitments"][sk])
+	# session["everything"] = genvote.challengeHash(''.join(map(str,[session["rc"]] + list(chain(session["cmt_list"])))), genvote.K)
+	# session["rx"] = genvote.order.random()
+	# session["x"] = genvote.commit(Bn.from_hex(session["everything"]), session["rx"])
+
+	# NOW WE NEED TO PRINT THE TWO LINES BEHIND THE SHIELD
+
+
+	# JUST FOR TESTING WHAT THE SERIALIZATION ISSUE IS 
+	# session["R"] = None
+	# session["R"] = hex(session["R"])
+	# print(type(session["rc"]))
+	# session["rc"] = hex(session["rc"])
+	# session["masks"] = None
+	# session["rb"] = None
+	# session["commitments"] = None
+	# session["randoms"] = None
+	# session["cmt_list"] = None
+	# session["everything"] = None
+	# session["rx"] = None
+	# session["x"] = None
+	# session["answers"] = None
+	# # session["challenge_dict"] = None
+	# session["receipt"] = None
+
 
 	return render_template("stage3.html", candidates = session["candidates"], cand_dict = session["rev_d"], chosen = session["chosen"], challenges = session["challenges"])
 
@@ -145,10 +279,12 @@ def stage3():
 # @app.route("/")
 def stage4():
 
+	print(session["challenges"])
+
 	# super annoying that I have to do this for some reason
 	reset_dict_keys()
-	# print("I'm in stage 4")
-	# print(session["challenges"])
+
+	print(session["challenges"])
 
 	chosen_challenge = " ".join(numpy.random.choice(session["dictionary_words"], session["lenChallenge"], replace = False))
 
@@ -162,17 +298,70 @@ def stage5():
 	# super annoying that I have to do this for some reason
 	reset_dict_keys()
 
-
 	session["challenges"][session["chosen"]] = request.form["chosen_challenge"]
+
+
+
+	# NEW
+	R, rc, masks, rb, commitments, randoms, cmt_list, everything, rx, x, answers, receipt = desist_tricky_objects()
+
+	answers = genvote.answerChallenges(session["challenges"], randoms, genvote.K, R)
+	genvote.verifyCommitment(x, rc, cmt_list, rx)
+	challenge_dict = {candidate: {'challenge': session["challenges"][candidate], 'answer': list(map(str,answers[candidate])), 'proof': commitments[candidate]} for candidate in session["challenges"]}
+	receipt = genvote.serializeEcPts({'voter_id': session["voter_id"], 'challenges': challenge_dict, 'vote_commitment': rc, 'rx': str(rx), 'commitment_to_everything': x})
+	sig = do_ecdsa_sign(genvote.G, genvote.sig_key, genvote.EcPtToStr(x).encode('utf-8'), genvote.kinv_rp)
+	signed_cmt = ' '.join((genvote.EcPtToStr(x), hex(sig[0])[2:], hex(sig[1])[2:]))
+	qr = qrcode.QRCode(
+			version = 1,
+			error_correction = qrcode.constants.ERROR_CORRECT_L,
+			box_size = 4,
+			border = 4,
+	)
+	qr.add_data(signed_cmt)
+	qr.make()
+	img = qr.make_image()
+	img.save()
+	img.save('qrcodes/to_print.png')
+	# REALLY, WE NEED TO PRINT THE QR CODE
+
+	persist_tricky_objects(R, rc, masks, rb, commitments, randoms, cmt_list, everything, rx, x, answers, receipt)
 
 	return render_template("stage5.html", candidates = session["candidates"], cand_dict = session["rev_d"], chosen = session["chosen"], challenges = session["challenges"])
 
 @app.route("/stage6")
 def stage6():
 
-	# store receipt to flat file
+	R, rc, masks, rb, commitments, randoms, cmt_list, everything, rx, x, answers, receipt = desist_tricky_objects()
+
+	# after confirming, store voter information
+	session["vote_data"].append((session["chosen"], rc, R, everything, str(x), answers, receipt))
 
 	return render_template("stage6.html")
+
+@app.route("/finish")
+def finish():
+
+	session["vote_data"].sort(key = lambda x: x[6]['voter_id'])
+	votes = [v[0] for v in vote_data]
+	vote_commits = [v[1] for v in vote_data]
+	randoms = [v[2] for v in vote_data]
+
+	receipts = [v[6] for v in vote_data]
+	genvote.verifyChallenge(receipts[0]['challenges'], receipts[0]['vote_commitment'])
+
+	tally = Counter(votes)
+	print(tally)
+
+	proofs = genvote.doFiatShamir(votes, vote_commits, randoms, tally)
+	big_dict = {'G': '934', 'sleeve': genvote.sleeve, 'g': genvote.EcPtToStr(g), 'h': genvote.EcPtToStr(h), 'precinct-id': '0', 'receipts': receipts, 'tally': tally, 'proofs': proofs}
+	json_str = json.dumps(big_dict)
+
+	for candidate in tally:
+		print("%s: %d" % (session["rev_d"][candidate], tally[candidate]))
+	with open("./verification.json", 'w') as f:
+		f.write(json_str)
+
+	return render_template("finished.html")
 
 if __name__ == "__main__":
     app.run(port = 5678, debug = True)
